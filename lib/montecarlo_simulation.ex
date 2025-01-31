9dbb7c2e709d825623115587e6347e5aa8c5c3a7
Tim Jarratt <tim.jarratt@maersk.com>
2025-01-31 18:10:18 +0100

    Setup livebook for experimentation

--- lib/montecarlo_simulation.ex
+++ lib/montecarlo_simulation.ex
@@ -1,18 +1,22 @@
-defmodule MontecarloSimulation do
-  @moduledoc """
-  Documentation for `MontecarloSimulation`.
-  """
+defmodule MonteCarloSimulation do
+  def forecast(days_thus_far, stories_remaining, tickets_per_week) do
+    stories_delivered = Enum.random(tickets_per_week)
 
-  @doc """
-  Hello world.
+    if stories_delivered >= stories_remaining do
+      days_thus_far + 5
+    else
+      forecast(days_thus_far + 5, stories_remaining - stories_delivered, tickets_per_week)
+    end
+  end
 
-  ## Examples
-
-      iex> MontecarloSimulation.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def percent(outcomes, number_simulations) do
+    outcomes
+    |> Enum.map(fn {_days_elapsed, occurrences} -> occurrences end)
+    |> Enum.sum()
+    |> Kernel./(number_simulations)
+    |> Kernel.*(100)
+    |> Float.round(2)
   end
 end
+
+
